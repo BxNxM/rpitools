9d9f1847ff286b1a4bade8d55b4db445a2ebc177
Bán Marcell <bnm@Bans-MacBook-Pro.local>
2017-12-01 16:10:33 +0100

    logging improve

--- sub_elapsed_time.bash
+++ sub_elapsed_time.bash
@@ -1,5 +1,7 @@
 
 function elapsed_time() {
+    local rpitools_log_path="cache/rpitools.log"
+
     option="$1"                 #start - stop
 
     if [ "$option" == "start" ]
@@ -10,15 +12,15 @@ function elapsed_time() {
     if [ "$option" == "stop" ]
     then
         duration=$SECONDS
-        echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+        echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed." | tee -a "$rpitools_log_path"
     fi
 
     if [ "$option" != "stop" ] && [ "$option" != "start" ]
     then
-        echo -e "INVALID OPTION: $option (VALID: start or stop)"
+        echo -e "INVALID OPTION: $option (VALID: start or stop)" | tee -a "$rpitools_log_path"
     fi
 }
 
-#elapsed_time "start"
-#sleep 5
-#elapsed_time "stop"
+elapsed_time "start"
+sleep 5
+elapsed_time "stop"
