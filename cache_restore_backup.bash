40d12eaf9af580d8534d3db4f087f5b02cf4e2cd
Bán Marcell <bnm@Bans-MacBook-Pro.local>
2017-12-08 23:51:41 +0100

    restore/backup done, process list updates

--- cache_restore_backup.bash
+++ cache_restore_backup.bash
@@ -3,7 +3,7 @@
 arg_len=$#
 arg_list=$@
 
-backup_path=~/rpitools_bckp/
+backup_path=~/.rpitools_bckp/
 
 # message handler function
 function message() {
@@ -12,8 +12,8 @@ function message() {
     local msg="$1"
     if [ ! -z "$msg" ]
     then
-        echo -e "$(date '+%Y.%m.%d %H:%M:%S') ${PURPLE}[ appinstall ]${NC} $msg"
-        echo -e "$(date '+%Y.%m.%d %H:%M:%S') ${PURPLE}[ appinstall ]${NC} $msg" >> "$rpitools_log_path"
+        echo -e "$(date '+%Y.%m.%d %H:%M:%S') ${PURPLE}[ cache backup/retore ]${NC} $msg"
+        echo -e "$(date '+%Y.%m.%d %H:%M:%S') ${PURPLE}[ cache backup/retore ]${NC} $msg" >> "$rpitools_log_path"
     fi
 }
 
