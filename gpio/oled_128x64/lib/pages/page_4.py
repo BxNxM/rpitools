3af28773bc3c578302589966b23f16af6a00b72b
Ban Marcell <ban.marcell@gmail.com>
2018-01-09 19:35:39 +0100

    interactive rgb page

--- gpio/oled_128x64/lib/pages/page_4.py
+++ gpio/oled_128x64/lib/pages/page_4.py
@@ -9,27 +9,31 @@ import random
 
 def page_setup(display):
     display.head_page_bar_switch(True, True)
-    display.display_refresh_time_setter(10)
+    display.display_refresh_time_setter(0.3)
 
 def page(display, ok_button):
     x = 0
     y =14
 
-    r_value = random.randint(0, 100)
-    g_value = random.randint(0, 100)
-    b_value = random.randint(0, 100)
-
-    cmd_aliad = "/home/$USER/rpitools/gpio/rgb_led/bin/rgb_interface.py -s ON -l ON -r {} -g {} -b {}".format(r_value, g_value, b_value)
-    #cmd_aliad = "rgbinterface  -r {} -g {} -b {}".format(r_value, g_value, b_value)
-    subprocess.call(cmd_aliad, shell=True)
-
     w, h = display.draw_text("LED:", x, y)
+    display.draw.rectangle((70,  30, 127, 50), outline=255, fill=0)
+    display.draw_text("Press OK", x+76, y*3 - 8)
     y += h
-    w, h = display.draw_text("    r: " + str(r_value) + " ", x, y)
-    y += h
-    w, h = display.draw_text("    g: " + str(g_value) + " ", x, y)
-    y += h
-    w, h = display.draw_text("    b: " + str(b_value) + " ", x, y)
+
+    if ok_button:
+        r_value = random.randint(0, 100)
+        g_value = random.randint(0, 100)
+        b_value = random.randint(0, 100)
+
+        w, h = display.draw_text("    r: " + str(r_value) + " ", x, y)
+        y += h
+        w, h = display.draw_text("    g: " + str(g_value) + " ", x, y)
+        y += h
+        w, h = display.draw_text("    b: " + str(b_value) + " ", x, y)
+
+        cmd_aliad = "/home/$USER/rpitools/gpio/rgb_led/bin/rgb_interface.py -s ON -l ON -r {} -g {} -b {}".format(r_value, g_value, b_value)
+        #cmd_aliad = "rgbinterface  -r {} -g {} -b {}".format(r_value, g_value, b_value)
+        subprocess.call(cmd_aliad, shell=True)
 
     return False
 
