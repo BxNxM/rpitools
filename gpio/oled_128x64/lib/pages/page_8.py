45e29f06797b8c0255476073852591138b7ed0c2
Ban Marcell <ban.marcell@gmail.com>
2018-01-09 13:20:35 +0100

    weather page beta1

--- gpio/oled_128x64/lib/pages/page_8.py
+++ gpio/oled_128x64/lib/pages/page_8.py
@@ -1,10 +1,21 @@
 import subprocess
 import time
+import os
+import sys
+try:
+    myfolder = os.path.dirname(os.path.abspath(__file__))
+    widget_module_path = os.path.dirname(myfolder)
+    sys.path.append(widget_module_path)
+    import oled_gui_widgets
+except Exception as e:
+    print("!"*100)
+    print(e)
+    print("!"*100)
 
 #################################################################################
-#                                PAGE 5 - empty page demo                       #
+#                                 PAGE 8 - WEATHER PAGE                         #
 #                              ----------------------------                     #
-#                                 * TEMP, * CPU freq                            #
+#                               *weather *temp *wind *rain                      #
 #################################################################################
 
 def page_setup(display):
@@ -12,8 +23,18 @@ def page_setup(display):
     display.display_refresh_time_setter(10)
 
 def page(display, ok_button):
-    location = "Budapest"
-    cmd = "curl wttr.in/Budapest"
+    weather_dict = oled_gui_widgets.get_weather_info()
+
+    x = 0
+    y = 14
+    # Write two lines of text.
+    w, h = display.draw_text(str(weather_dict["weather"]), x, y)
+    y+=h
+    display.draw_text("temp:  " + str(weather_dict["temp"][0:3]) + " 'C", x, y)
+    y+=h
+    display.draw_text("wind:  " + str(weather_dict["wind"]), x, y)
+    y+=h
+    display.draw_text("rain:  " + str(weather_dict["rain"]), x, y)
     return False
 
 def page_destructor(display):
