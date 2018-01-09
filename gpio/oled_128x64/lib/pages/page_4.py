7b9a9ea93d6e4d3ba1c2a5e068f8e175a253853e
Ban Marcell <ban.marcell@gmail.com>
2018-01-10 00:34:53 +0100

    image page extended with more images

--- gpio/oled_128x64/lib/pages/page_4.py
+++ gpio/oled_128x64/lib/pages/page_4.py
@@ -20,7 +20,7 @@ def page_setup(display):
 
 def page(display, ok_button):
     global pics_index, stored_index
-    image_list = [ 'pages/images/rpi.png', 'pages/images/happycat_oled_64.ppm']
+    image_list = [ 'pages/images/rpi.png', 'pages/images/katica_draw.png', 'pages/images/linux.png', 'pages/images/happycat_oled_64.ppm']
     if ok_button:
         pics_index += 1
         if pics_index >= len(image_list):
@@ -28,6 +28,12 @@ def page(display, ok_button):
 
     if stored_index != pics_index:
         stored_index = pics_index
+        # set page setup with picture index 1 and so on...
+        if stored_index == 1 or stored_index == 2:
+            display.head_page_bar_switch(False, True)
+        else:
+            # else set backup to original
+            display.head_page_bar_switch(True, True)
         display.draw_image(image_list[pics_index])
     return True
 
