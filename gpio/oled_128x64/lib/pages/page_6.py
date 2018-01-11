250d70268237f8536cbc05f5f72b1730c2f83449
BxNxM <ban.marcell@gmail.com>
2018-01-12 00:22:34 +0100

    standby page improved

--- gpio/oled_128x64/lib/pages/page_6.py
+++ gpio/oled_128x64/lib/pages/page_6.py
@@ -6,16 +6,17 @@ import time
 #                              ----------------------------                     #
 #                                                                               #
 #################################################################################
-state = False
+is_first_load = True
 
 def page_setup(display):
-    global state
-    state = False
+    global is_first_load
+    is_first_load = True
     display.head_page_bar_switch(True, True)
     display.display_refresh_time_setter(1)
 
 def page(display, ok_button):
-    global state
+    global is_first_load
+
     x = 0
     y = 15
     # Write two lines of text.
@@ -24,12 +25,18 @@ def page(display, ok_button):
     w, h = display.draw_text("Press OK", x+35, y)
     y+=h + 2
 
-    if ok_button and state is False:
-        state = True
-        display.standby_switch(mode=True)
-    elif ok_button and state is True:
+    if display.standby is False and not is_first_load:
         state = False
+        display.draw_text("oled is ready...", x+35, y)
         display.standby_switch(mode=False)
+    if ok_button and display.standby is False:
+        state = True
+        display.draw_text("going standby...", x+35, y)
+        display.display_show()
+        time.sleep(1)
+        y+=h + 2
+        display.standby_switch(mode=True)
+        is_first_load = not is_first_load
 
     return False
 
