ef9eb14e3f269436456d2561553d39ae41ca9009
Ban Marcell <ban.marcell@gmail.com>
2018-01-13 23:47:57 +0100

    omxplayer finished - esaygui - console autodetect mode with maunal forced options

--- tools/omxplayer_gui/omx_gui.py
+++ tools/omxplayer_gui/omx_gui.py
@@ -5,15 +5,53 @@ import easygui
 import os
 import subprocess
 import sys
+import argparse
 
 videos_path = "/home/pi/Videos/"
 
+# argparser for manual start
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--gui",  action='store_true', help="run omcplayer with easygui")
+parser.add_argument("-c", "--console",  action='store_true', help="run omxplayer with console")
+args = parser.parse_args()
+force_gui = args.gui
+force_console = args.console
+is_manual_settings = False
+if (force_gui or force_console) and not (force_gui is True and force_console is True):
+    is_manual_settings = True
+
+# easygui function
 def run_gui(choices):
     msg ="Which movie do you want to play?\nTo exit press cancel."
     title = "OMXPLAYER EASYGUI INTERFACE"
     choice = easygui.choicebox(msg, title, choices)
     return choice
 
+# console function
+def run_console(choices):
+    msg ="Which movie do you want to play?\nTo exit press cancel."
+    title = "OMXPLAYER CONSOLE INTERFACE"
+
+    for index, movie_name in enumerate(choices):
+        print("[ {} ] - {}".format(index, movie_name))
+    print(title)
+    print(msg)
+    try:
+        selected_index = int(input("Choose one index from the list! "))
+    except KeyboardInterrupt:
+        selected_index = None
+        print("Goodbye!")
+        sys.exit(0)
+    except ValueError:
+        return run_console(choices)                             # recursive cal!!!
+    except Exception as e:
+        selected_index = None
+        print("Exception: " + str(e))
+        sys.exit(1)
+    choice = choices[selected_index]
+    return choice
+
+# list movies from the given folder - videos_path
 def list_movies():
     videos_path_dict = {}
     global videos_path
@@ -22,26 +60,40 @@ def list_movies():
         #print(dirs)
         #print(files)
         for actual_file in files:
-            if ".mkv" in actual_file or ".avi" in actual_file:
+            if (".mkv" in actual_file or ".avi" in actual_file) and "simple" not in actual_file:
                 path = os.path.join(root, actual_file)
                 videos_path_dict[actual_file] = path
                 print("Movie found: " + str(path))
     return videos_path_dict
 
-def play_selected_movie(movies_dict, selected):
+# play selected movie with omxplayer
+def play_selected_movie(movies_dict, selected, is_easygui=True):
     if selected is None:
         print("Goodbye!")
         sys.exit(0)
+    # get selected movie path
     selected_movie = movies_dict[selected]
-    if easygui.ccbox("Play " + str(selected_movie) + " ?", "Movie to play"):
-        cmd = "/usr/bin/omxplayer -o hdmi " + str(selected_movie)
-        print(cmd)
-        output = subprocess.check_output(cmd, shell = True)
-        print(output)
+    #use with esaygui
+    if is_easygui:
+        if easygui.ccbox("Play " + str(selected_movie) + " ?", "Movie to play"):
+            cmd = "/usr/bin/omxplayer -o hdmi " + str(selected_movie)
+            print(cmd)
+            output = subprocess.check_output(cmd, shell = True)
+            print(output)
+        else:
+            main_w_gui()
     else:
-        main()
+        # use with console
+        if str(input("Play " + str(selected_movie) + " ?\nMovie to play[Y/N] ")).lower() == "y":
+            cmd = "/usr/bin/omxplayer -o hdmi " + str(selected_movie)
+            print(cmd)
+            output = subprocess.check_output(cmd, shell = True)
+            print(output)
+        else:
+            main_w_console()
 
-def main():
+# main with easygui
+def main_w_gui():
     print("Start OMXPLAYER GUI")
     # search movies in the given folder
     movies_dict = list_movies()
@@ -52,5 +104,30 @@ def main():
     # run movie
     play_selected_movie(movies_dict, output)
 
+# main with console
+def main_w_console():
+    # search movies in the given folder
+    movies_dict = list_movies()
+    # create choice list
+    choices = list(movies_dict.keys())
+    # call choicebox
+    output = run_console(choices)
+    # run movie
+    play_selected_movie(movies_dict, output, is_easygui=False)
+
+# main block
 if __name__ == "__main__":
-    main()
+    if is_manual_settings:
+        print("Run with manual mode")
+        if force_gui:
+            main_w_gui()
+        if force_console:
+            main_w_console()
+    else:
+        print("Run autodetect mode")
+        # X is running, DISPLAY ENV is set
+        if "DISPLAY" in os.environ:
+            main_w_gui()
+        else:
+            # run with console gui - X and DISPLAY enc is not exists
+            main_w_console()
