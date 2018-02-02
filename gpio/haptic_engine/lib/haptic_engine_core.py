6b9a9a740873f102dd9d96e81a9eec76f8d77261
Ban Marcell <ban.marcell@gmail.com>
2018-02-02 22:12:01 +0100

    haptic engine core dome with UP - DOWN - SOFT - TAP - DoubleTAP - SNOOZE

--- gpio/haptic_engine/lib/haptic_engine_core.py
+++ gpio/haptic_engine/lib/haptic_engine_core.py
@@ -11,7 +11,7 @@ import time
 
 class OutputChannelDriver():
 
-    def __init__(self, channel=12):
+    def __init__(self, channel=11):
         self.channel = channel
 
         # SET GPIO MODE
@@ -27,7 +27,7 @@ class OutputChannelDriver():
 
     def set_state(self, state, time_sec=None):
         GPIO.output(self.channel, state)
-        if time is not None:
+        if time_sec is not None:
             time.sleep(time_sec)
 
     def soft_pwm(self, high_percent, hold_time_sec=0):
@@ -37,14 +37,14 @@ class OutputChannelDriver():
             self.set_state(state=True, time_sec=timing * high_percent)
             self.set_state(state=False, time_sec=timing * (100 - high_percent))
 
-    def gradient(self, start, end, hold_time=0.1):
+    def gradient(self, start, end, hold_time=0.1, step=8):
         if start < end:
-            for state in range(start, end, 8):
+            for state in range(start, end, step):
                 self.soft_pwm(state, hold_time)
             if state != end:
                 self.soft_pwm(end, hold_time)
         elif start > end:
-            for state in range(start, end, -8):
+            for state in range(start, end, -step):
                 self.soft_pwm(state, hold_time)
             if state != end:
                 self.soft_pwm(end, hold_time)
@@ -54,34 +54,72 @@ class OutputChannelDriver():
 
 class HapticEngine(OutputChannelDriver):
 
-    def __init__(self, channel=12):
-        super().__init__(self, channel)
+    def __init__(self, channel=11, speed=18):
+        super().__init__(channel)
+        self.speed = 1 / speed
 
     def UP(self):
-        pass
+        self.gradient(5, 100, hold_time=self.speed, step=15)
+        self.set_state(True, time_sec=0.05)
+        self.set_state(False)
 
     def DOWN(self):
-        pass
+        self.set_state(True, time_sec=0.05)
+        self.gradient(100, 5, hold_time=self.speed, step=15)
+        self.set_state(False)
 
-    def LOOP(self):
-        pass
+    def SOFT(self):
+        self.gradient(5, 100, hold_time=self.speed, step=30)
+        self.gradient(100, 5, hold_time=self.speed, step=30)
+        self.set_state(False)
 
     def TAP(self):
-        pass
+        self.gradient(90, 100, hold_time=self.speed, step=10)
+        self.set_state(False)
+
+    def DoubleTAP(self):
+        self.TAP()
+        time.sleep(0.1)
+        self.TAP()
+
+    def SNOOZE(self):
+        self.gradient(0, 100, hold_time=self.speed*5, step=5)
+        self.set_state(False)
 
 def OutputChannelDriver_DEMO():
-    hapt = OutputChannelDriver()
+    driver = OutputChannelDriver()
     print("set state")
-    hapt.set_state(True, time_sec=0.2)
+    driver.set_state(True, time_sec=0.2)
     time.sleep(0.5)
     print("soft_pwm")
-    hapt.soft_pwm(30, 4)
+    driver.soft_pwm(30, 4)
     time.sleep(0.5)
     print("gradient 0 -> 100")
-    hapt.gradient(0, 100)
+    driver.gradient(0, 100)
     time.sleep(0.5)
     print("gradient 100 -> 0")
-    hapt.gradient(100, 0)
+    driver.gradient(100, 0)
+
+def HapticEngine_DEMO():
+    hapt = HapticEngine()
+    print("HapticEngine: UP")
+    hapt.UP()
+    time.sleep(1)
+    print("HapticEngine: DOWN")
+    hapt.DOWN()
+    time.sleep(1)
+    print("HapticEngine: SOFT")
+    hapt.SOFT()
+    time.sleep(1)
+    print("HapticEngine: TAP")
+    hapt.TAP()
+    time.sleep(1)
+    print("HapticEngine: DoubleTAP")
+    hapt.DoubleTAP()
+    time.sleep(1)
+    print("HapticEngine: SNOOZE")
+    hapt.SNOOZE()
 
 if __name__ == "__main__":
     OutputChannelDriver_DEMO()
+    HapticEngine_DEMO()
