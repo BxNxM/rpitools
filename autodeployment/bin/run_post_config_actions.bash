b713cacb9a7b5b58769cd364dc0910096f5f5887
BxNxM <ban.marcell@gamil.com>
2018-03-01 23:36:51 +0000

    autodeployment pixel and vnc install based on config

--- autodeployment/bin/run_post_config_actions.bash
+++ autodeployment/bin/run_post_config_actions.bash
@@ -3,9 +3,27 @@
 MYPATH_="${BASH_SOURCE[0]}"
 MYDIR_="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${MYDIR_}/../../prepare/colors.bash"
+confighandler="/home/$USER/rpitools/autodeployment/bin/ConfigHandlerInterface.py"
 
 configure_transmission="${MYDIR_}/../lib/configure_transmission.bash"
 
 echo -e "${YELLOW}RUN: configure_transmission ${NC}"
 . "$configure_transmission"
 
+pixel_install="$($confighandler -s INSTALL_PIXEL -o action)"
+if [ "$pixel_install" == "True" ]
+then
+    echo -e "${YELLOW}RUN: install PIXEL install ${NC}"
+    . "${MYDIR_}/../../prepare/system/install_PIXEL.bash"
+else
+    echo -e "${YELLOW}PIXEL install is not requested ${NC}"
+fi
+
+vnc_install="$($confighandler -s INSTALL_VNC -o action)"
+if [ "$vnc_install" == "True" ]
+then
+    echo -e "${YELLOW}RUN: install VNC install ${NC}"
+    . "${MYDIR_}/../../prepare/system/install_vnc.bash"
+else
+    echo -e "${YELLOW}VNC install is not requested ${NC}"
+fi
