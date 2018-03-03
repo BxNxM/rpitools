84f72d79fdf3eb50dcca8f42764c2c441ef963e1
Bán Marcell <bnm@Bans-MacBook-Pro.local>
2018-03-03 21:07:36 +0100

    autodeployment/lib/set_i2c_raspberrypi.conf.bash fix

--- autodeployment/lib/set_i2c_raspberrypi.conf.bash
+++ autodeployment/lib/set_i2c_raspberrypi.conf.bash
@@ -3,18 +3,21 @@
 conf_file_path="/etc/modules-load.d/raspberrypi.conf"
 if [ ! -e "$conf_file_path" ]
 then
-    echo "" > "$conf_file_path"
+    echo -e "Create file $conf_file_path (sudo echo \"\" > \"$conf_file_path\")"
+    sudo /usr/bin/touch "$conf_file_path"
+    sudo chmod 666 "$conf_file_path"
 fi
 
 function add_if_not_added() {
     local parameter="$1"
-    local is_added=$(grep -rnw "$conf_file_path" -e "$parameter")
+    local is_added=$(sudo grep -rnw "$conf_file_path" -e "$parameter")
     if [ "$is_added" == "" ]
     then
         echo -e "Add $parameter to $conf_file_path"
-        echo "$parameter" >> "$conf_file_path"
+        sudo echo "$parameter" >> "$conf_file_path"
     fi
 }
 
+sudo chmod 666 "$conf_file_path"
 add_if_not_added "i2c-bcm2708"
 add_if_not_added "i2c-dev"
