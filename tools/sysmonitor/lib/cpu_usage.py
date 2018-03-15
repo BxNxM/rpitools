5ed01acdf8f888a01276785a9e8d8382cb1862e0
BxNxM <ban.marcell@gmail.com>
2018-03-15 11:06:14 +0000

    sysmonitor - memory and cpu view improved

--- tools/sysmonitor/lib/cpu_usage.py
+++ tools/sysmonitor/lib/cpu_usage.py
@@ -12,11 +12,22 @@ def get_cpu_usage():
     data = data[6:-6]
     return int(data)
 
+def get_top_processes(topx=3):
+    output = ""
+    cmd = "ps aux | head -n 1; ps aux --sort -rss | sort -nrk 3,3 | head -n " + str(topx)
+    data = LocalMachine.run_command_safe(cmd)
+    data_list = data.split("\n")
+    for line in data_list:
+        output += " " + line + "\n"
+    return output
+
 def create_printout(separator="|", char_width=80):
     text = GeneralElements.header_bar(" CPU USAGE ", char_width, separator, color_name=Colors.LIGHT_PURPLE)
     cpu_usage = get_cpu_usage()
 
     text += GeneralElements.indicator_bar(cpu_usage, dim="%", pre_text="CPU", char_width=char_width, col_scale=[0.75, 0.90])
+    text += get_top_processes()
+
     return text
 
 def main():
--- tools/sysmonitor/lib/mem_usage.py
+++ tools/sysmonitor/lib/mem_usage.py
@@ -11,16 +11,21 @@ def get_mem_usage():
     mem_total = LocalMachine.run_command_safe("sudo cat /proc/meminfo | grep 'MemTotal' | tr -dc '0-9'")
     mem_available = LocalMachine.run_command_safe("sudo cat /proc/meminfo | grep 'MemAvailable' | tr -dc '0-9'")
     mem_free = LocalMachine.run_command_safe("sudo cat /proc/meminfo | grep 'MemFree' | tr -dc '0-9'")
+
     available_percent = (float(int(mem_total) - int(mem_available)) / float(mem_total)) * 100
     available_percent = '%.1f' % available_percent
-    return float(available_percent), mem_total, mem_available, mem_free
+
+    free_percent = (float(int(mem_total) - int(mem_free)) / float(mem_total)) * 100
+    free_percent = '%.1f' % free_percent
+    return float(available_percent), float(free_percent), mem_total, mem_available, mem_free
 
 def create_printout(separator="|", char_width=80):
     text = GeneralElements.header_bar(" MEM USAGE ", char_width, separator, color_name=Colors.LIGHT_CYAN)
-    mem_usage_percent, total, available, free = get_mem_usage()
+    mem_usage_av_percent, mem_usage_free_percent, total, available, free = get_mem_usage()
 
     text += " Total: {} Mb Available: {} Mb Free: {} Mb\n".format(int(total)/1024, int(available)/1024, int(free)/1024)
-    text += GeneralElements.indicator_bar(mem_usage_percent, dim="%", pre_text="MEM", char_width=char_width)
+    text += GeneralElements.indicator_bar(mem_usage_av_percent, dim="%", pre_text="MEM AV. ", char_width=char_width)
+    text += GeneralElements.indicator_bar(mem_usage_free_percent, dim="%", pre_text="MEM FREE", char_width=char_width)
     return text
 
 def main():
