6fc5636877a86bfdd1751febdb8e8a001b225412
BxNxM <ban.marcell@gmail.com>
2018-03-20 16:56:42 +0000

    dropbox halpage louncher fixed

--- tools/dropbox_halpage/bin/halpage_louncher.bash
+++ tools/dropbox_halpage/bin/halpage_louncher.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+#https://github.com/andreafabrizi/Dropbox-Uploader
+
 MYPATH="${BASH_SOURCE[0]}"
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 is_running="$(ps aux | grep -v grep | grep 'dropbox_ext_ip_sync.bash')"
 if [ "$is_running" == "" ]
 then
-    . "${MYDIR}/../lib/dropbox_ext_ip_sync.bash"
+    ("${MYDIR}/../lib/dropbox_ext_ip_sync.bash" &)
 else
     echo -e "[ DROPBOX HALPAGE ] already running"
 fi
