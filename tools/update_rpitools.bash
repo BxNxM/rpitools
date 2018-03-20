9b3891f97e0a1a0c8674020661e1f641c9050dbf
BxNxM <ban.marcell@gmail.com>
2018-03-20 21:04:27 +0000

    stop start services under updrade

--- tools/update_rpitools.bash
+++ tools/update_rpitools.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+arg_len="$#"
+option="$1"
+if [ "$arg_len" -eq 1 ]
+then
+    if [ "$option" != "start" ] && [ "$option" != "stop" ]
+    then
+        option="stop"
+        echo -e "[ WARNING ] - wrong given option $option -> start/stop"
+    fi
+else
+    echo -e "[ WARNING ] - option is not given -> start/stop"
+fi
+
 function stop_running_processes() {
     process_list=("oled_gui_core")
     for process in "${process_list[@]}"
@@ -23,6 +36,38 @@ function stop_running_processes() {
     done
 }
 
-echo -e "STOP PROCESSES"
-stop_running_processes
-echo -e "UPDATE"
+function start_running_processes() {
+    process_list=("oled_gui_core")
+    for process in "${process_list[@]}"
+    do
+        is_run=$(ps aux | grep "$process" | grep -v grep)
+        if [ "$is_run" == "" ]
+        then
+            echo -e "sudo systemctl start $process"
+            sudo systemctl start "$process"
+        fi
+    done
+    process_list=("dropbox_halpage")
+    for process in "${process_list[@]}"
+    do
+        is_run=$(ps aux | grep "$process" | grep -v grep)
+        if [ "$is_run" == "" ]
+        then
+            echo -e "sudo systemctl start $process"
+            sudo systemctl start "$process"
+        fi
+    done
+}
+
+if [ "$option" == "stop" ]
+then
+    echo -e "STOP PROCESSES"
+    stop_running_processes
+    echo -e "UPDATE"
+elif [ "$option" == "start" ]
+then
+    echo -e "START PROCESSES"
+    start_running_processes
+    echo -e "DONE"
+fi
+
