1a2979f4c735fd861d2e0206eee0f0e310f64126
BxNxM <ban.marcell@gmail.com>
2018-03-21 16:36:20 +0000

    safe lounch dropbox haloage service fixed

--- tools/dropbox_halpage/bin/halpage_louncher.bash
+++ tools/dropbox_halpage/bin/halpage_louncher.bash
@@ -9,10 +9,17 @@ else
     lounch_mode=0
 fi
 
+if [ "$lounch_mode" -eq 0 ]
+then
+    service_is_run="$(systemctl is-active dropbox_halpage)"
+else
+    service_is_run="if_service_call_arg_on_cant_check_service_state"
+fi
+
 MYPATH="${BASH_SOURCE[0]}"
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 is_running="$(ps aux | grep -v grep | grep 'dropbox_ext_ip_sync.bash')"
-if [ "$is_running" == "" ]
+if [ "$is_running" == "" ] && [ "$service_is_run" != "active" ]
 then
     if [ "$lounch_mode" == 0 ]
     then
