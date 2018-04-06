0fa1b2b6337bee48b0f5ce9cd908cd3d53876c0f
ban.marcell@gamil.com <BxNxM>
2018-04-06 23:24:53 +0200

    conky autoset based on rpitools_config.cfg model type

--- tools/set_conky/conky.bash
+++ tools/set_conky/conky.bash
@@ -3,6 +3,16 @@
 # more info about conky: https://www.novaspirit.com/2017/02/23/desktop-widget-raspberry-pi-using-conky/
 MYPATH="${BASH_SOURCE[0]}"
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+confighandler="/home/$USER/rpitools/autodeployment/bin/ConfigHandlerInterface.py"
+rpimodel="$($confighandler -s RPI_MODEL -o model)"
+conky_conf_pi_zero=".conkyrc_pizero"
+conky_conf_pi_3=".conkyrc_pi3"
+if [ "$rpimodel" == "rpi_zero" ]
+then
+    conky_conf_to_set="$conky_conf_pi_zero"
+else
+    conky_conf_to_set="$conky_conf_pi_3"
+fi
 
 # set DISPLAY=:0 if xinit is run
 xinitrx_is_run=$(ps aux | grep "[x]initrc")
@@ -16,8 +26,8 @@ fi
 # check conky config
 if [ ! -e ~/.conkyrc ]
 then
-    echo -e "Copy conly configuration: ${MYDIR}/.conkyrc -> ~/.conkyrc"
-    cp "${MYDIR}/.conkyrc" ~/.conkyrc
+    echo -e "Copy conky configuration: ${MYDIR}/${conky_conf_to_set} -> ~/.conkyrc"
+    cp "${MYDIR}/${conky_conf_to_set}" ~/.conkyrc
 else
     echo -e "~/.conkyrc is already set"
 fi
