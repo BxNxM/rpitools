6c320798830206c78f6c49889c29845dbe5e0a4e
ban.marcell@gamil.com <BxNxM>
2018-08-30 14:42:28 +0000

    [BETA] universal service template with configuration done
        * memDictCore adapted

--- tools/socketmem/systemd_setup/set_service.bash
+++ tools/socketmem/systemd_setup/set_service.bash
@@ -2,10 +2,12 @@
 
 MYPATH="${BASH_SOURCE[0]}"
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+service_template_path="/home/$USER/rpitools/template/general_template.service"
 
 confighandler="/home/$USER/rpitools/autodeployment/bin/ConfigHandlerInterface.py"
+rpitools_linux_user="$($confighandler -s GENERAL -o user_name_on_os)"
 set_service_conf="True"
+service_name="memDictCore.service"
 
 function message() {
     local msg="$1"
@@ -13,6 +15,43 @@ function message() {
     then
         echo -e "$(date '+%Y.%m.%d %H:%M:%S') [ SET SYSTEMD SERVICE ] $msg"
     fi
+}
+
+function change_parameter() {
+    local from="$1"
+    local to="$2"
+    local where="$3"
+    if [ ! -z "$from" ]
+    then
+        is_set="$(sudo cat "$where" | grep -v grep | grep "$from")"
+        message "sudo cat $where | grep -v grep | grep $from\nis_set: $is_set"
+        message "$is_set"
+        if [ "$is_set" != "" ]
+        then
+            message "${GREEN}Set parameter: $to  (from: $from) ${NC}"
+            sudo sed -i 's|'"${from}"'|'"${to}"'|g' "$where"
+        else
+            message "${GREEN}Custom parameter $from not exists in $where ${NC}"
+        fi
+    fi
+}
+
+function create_service() {
+    local service_description="socket memory dict service louncher"
+    local exec_cmd="/bin/bash memDictCore.bash"
+    local working_dir="/home/${rpitools_linux_user}/rpitools/tools/socketmem/bin/"
+    local syslog_idf="memDictCore"
+    local user="${rpitools_linux_user}"
+
+    message "Copy service template ${service_template_path} -> ${MYDIR}/${service_name}"
+    cp "${service_template_path}" "${MYDIR}/${service_name}"
+
+    message "Set servive temaplete"
+    change_parameter "%DESCRIPTION%" "${service_description}" "${MYDIR}/${service_name}"
+    change_parameter "%INTERPRETER_n_SCRIPTNAME%" "${exec_cmd}" "${MYDIR}/${service_name}"
+    change_parameter "%SCRIPT_PATH%" "${working_dir}" "${MYDIR}/${service_name}"
+    change_parameter "%SYSLOG_IDF%" "${syslog_idf}" "${MYDIR}/${service_name}"
+    change_parameter "%USER%" "${user}" "${MYDIR}/${service_name}"
 }
 
 function check_exitcode() {
@@ -25,7 +64,7 @@ function check_exitcode() {
 }
 
 function function_demo() {
-    service="memDictCore.service"
+    service="${service_name}"
     message "INFO about service (systemd)"
     message "systemctl status $service"
     message "systemctl is-active $service"
@@ -42,44 +81,45 @@ function function_demo() {
 
 if [ "$set_service_conf" == "True" ] || [ "$set_service_conf" == "true" ]
 then
-    echo -e "dropbox halpage service is required - turn on"
-    if [ ! -e "/lib/systemd/system/memDictCore.service" ]
+    echo -e "${service_name} service is required - turn on"
+    if [ ! -e "/lib/systemd/system/${service_name}" ]
     then
-        message "COPY: ${MYDIR}/memDictCore.service -> /lib/systemd/system/memDictCore.service"
-        sudo cp "${MYDIR}/memDictCore.service" "/lib/systemd/system/memDictCore.service"
+        create_service
+        message "COPY: ${MYDIR}/${service_name} -> /lib/systemd/system/${service_name}"
+        sudo cp "${MYDIR}/${service_name}" "/lib/systemd/system/${service_name}"
         check_exitcode "$?"
     else
-        message "/lib/systemd/system/memDictCore.service is already exists"
+        message "/lib/systemd/system/${service_name} is already exists"
         #function_demo
     fi
 
     if [ "$(systemctl is-active memDictCore)" == "inactive" ]
     then
-        message "START SERICE: sudo systemctl start memDictCore.service"
-        sudo systemctl start memDictCore.service
+        message "START SERICE: sudo systemctl start ${service_name}"
+        sudo systemctl start "${service_name}"
         check_exitcode "$?"
     else
-        message "ALREADY RUNNING SERICE: memDictCore.service"
+        message "ALREADY RUNNING SERICE: ${service_name}"
     fi
 
     if [ "$(systemctl is-enabled memDictCore)" == "disabled" ]
     then
-        message "ENABLE SERICE: sudo systemctl enable memDictCore.service"
-        sudo systemctl enable memDictCore.service
+        message "ENABLE SERICE: sudo systemctl enable ${service_name}"
+        sudo systemctl enable "${service_name}"
         check_exitcode "$?"
     else
-        message "SERICE IS ALREADY ENABLED: memDictCore.service"
+        message "SERICE IS ALREADY ENABLED: ${service_name}"
     fi
 
 elif [ "$set_service_conf" == "False" ] || [ "$set_service_conf" == "false" ]
 then
-    if [ ! -e "/lib/systemd/system/memDictCore.service" ]
+    if [ ! -e "/lib/systemd/system/${service_name}" ]
     then
-        message "COPY: ${MYDIR}/memDictCore.service -> /lib/systemd/system/memDictCore.service"
-        sudo cp "${MYDIR}/memDictCore.service" "/lib/systemd/system/memDictCore.service"
+        message "COPY: ${MYDIR}/${service_name} -> /lib/systemd/system/${service_name}"
+        sudo cp "${MYDIR}/${service_name}" "/lib/systemd/system/${service_name}"
         check_exitcode "$?"
     else
-        message "/lib/systemd/system/memDictCore.service is already exists"
+        message "/lib/systemd/system/${service_name} is already exists"
         #function_demo
     fi
 
@@ -87,20 +127,20 @@ then
 
     if [ "$(systemctl is-active memDictCore)" == "active" ]
     then
-        message "STOP SERICE: sudo systemctl stop memDictCore.service"
-        sudo systemctl stop memDictCore.service
+        message "STOP SERICE: sudo systemctl stop ${service_name}"
+        sudo systemctl stop "${service_name}"
         check_exitcode "$?"
     else
-        message "SERICE NOT RUNNING: memDictCore.service"
+        message "SERICE NOT RUNNING: ${service_name}"
     fi
 
     if [ "$(systemctl is-enabled memDictCore)" == "enabled" ]
     then
-        message "DISABLE SERICE: sudo systemctl disable memDictCore.service"
-        sudo systemctl disable memDictCore.service
+        message "DISABLE SERICE: sudo systemctl disable ${service_name}"
+        sudo systemctl disable "${service_name}"
         check_exitcode "$?"
     else
-        message "SERICE IS ALREADY DISBALED: memDictCore.service"
+        message "SERICE IS ALREADY DISBALED: ${service_name}"
     fi
 else
     echo -e "dropbox halpage service is not requested"
