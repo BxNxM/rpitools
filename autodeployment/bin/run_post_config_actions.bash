209438a3256c3c8ed6519192e7022c99ff7828b4
ban.marcell@protonmail.com <BxNxM>
2018-09-23 08:59:00 +0000

    User script validation done, and run_post_config_actions shellcheck-ed :D

--- autodeployment/bin/run_post_config_actions.bash
+++ autodeployment/bin/run_post_config_actions.bash
@@ -30,36 +30,36 @@ function post_config_actions_done() {
 
 # nfs client setup
 _msg_ "RUN: nfs client setup"
-(. "/home/$USER/rpitools/autodeployment/lib/configure_nfs_clinet.bash")
+("/home/$USER/rpitools/autodeployment/lib/configure_nfs_clinet.bash")
 
 # set media stream with minidlna
 _msg_ "RUN: minidlna setup"
-(. /home/$USER/rpitools/autodeployment/lib/configure_minidlna.bash)
+("/home/$USER/rpitools/autodeployment/lib/configure_minidlna.bash")
 
 # set nfs server
 _msg_ "RUN: nfs setup"
-(. /home/$USER/rpitools/autodeployment/lib/configure_nfs_server.bash)
+("/home/$USER/rpitools/autodeployment/lib/configure_nfs_server.bash")
 
 # set samba configuration
 _msg_ "RUN: configure_samba"
-(. "$configure_samba")
+("$configure_samba")
 
 # set memdict core service
 _msg_ "RUN: set up service if needed - memDictCore"
-(. "/home/$USER/rpitools/tools/socketmem/systemd_setup/set_service.bash")
+("/home/$USER/rpitools/tools/socketmem/systemd_setup/set_service.bash")
 
 # transmission install config executor
 _msg_ "RUN: configure_transmission"
-(. "$configure_transmission")
+("$configure_transmission")
 _msg_ "RUN: set transmission autorestart edit whitelist"
-(. "$set_transmission_whitelist_autoedit")
+("$set_transmission_whitelist_autoedit")
 
 # pixel install config executor
 pixel_install="$($confighandler -s INSTALL_PIXEL -o activate)"
 if [ "$pixel_install" == "True" ] || [ "$pixel_install" == "true" ]
 then
     _msg_ "RUN: PIXEL install"
-    (. ${MYDIR_}/../../prepare/system/install_PIXEL.bash)
+    ("${MYDIR_}/../../prepare/system/install_PIXEL.bash")
 else
     _msg_ "PIXEL install is not requested"
 fi
@@ -69,29 +69,29 @@ vnc_install="$($confighandler -s INSTALL_VNC -o activate)"
 if [ "$vnc_install" == "True" ] || [ "$vnc_install" == "true" ]
 then
     _msg_ "RUN: VNC install"
-    (. ${MYDIR_}/../../prepare/system/install_vnc.bash)
+    ("${MYDIR_}/../../prepare/system/install_vnc.bash")
 else
     _msg_ "VNC install is not requested"
 fi
 
 # set oled gui core service if config requires
 _msg_ "RUN: OLED GUI CORE service setup"
-(. "/home/$USER/rpitools/gpio/oled_128x64/systemd_setup/set_service.bash")
+("/home/$USER/rpitools/gpio/oled_128x64/systemd_setup/set_service.bash")
 
 # run kodi config settings
 _msg_ "RUN: KODI dektop icon and bootup start if set"
-(. /home/$USER/rpitools/autodeployment/lib/kodi_runner.bash)
+("/home/$USER/rpitools/autodeployment/lib/kodi_runner.bash")
 
 # set dropbox halpage service if config requires
 _msg_ "RUN: DROPBOX HALPAGE service setup"
-(. "/home/$USER/rpitools/tools/dropbox_halpage/systemd_setup/set_service.bash")
+("/home/$USER/rpitools/tools/dropbox_halpage/systemd_setup/set_service.bash")
 
 # Configure git
-(. "/home/$USER/rpitools/autodeployment/lib/configure_git.bash")
+("/home/$USER/rpitools/autodeployment/lib/configure_git.bash")
 
 # Custom logrotate
 _msg_ "RUN: custom logrotate service: log_cleaner.bash or rpitools_logrotate.service"
-(. /home/$USER/rpitools/tools/log_cleaner_service/systemd_setup/set_service.bash)
+("/home/$USER/rpitools/tools/log_cleaner_service/systemd_setup/set_service.bash")
 
 # set custom apache webpage
 _msg_ "RUN apache basic setup based on rpi_config and rpitools/autodeployment/lib/apache_setup/template"
@@ -99,22 +99,22 @@ apache_override_underupdate_webpage="$($confighandler -s APACHE -o override_unde
 if [[ "$apache_override_underupdate_webpage" == "True" ]] || [[ "$apache_override_underupdate_webpage" == "true" ]]
 then
     _msg_ "\t override_underupdate: $apache_override_underupdate_webpage"
-    (. /home/$USER/rpitools/autodeployment/lib/apache_setup/setup_based_on_template.bash -f)
+    ("/home/$USER/rpitools/autodeployment/lib/apache_setup/setup_based_on_template.bash -f")
 else
     _msg_ "\t override_underupdate: $apache_override_underupdate_webpage"
-    (. /home/$USER/rpitools/autodeployment/lib/apache_setup/setup_based_on_template.bash)
+    ("/home/$USER/rpitools/autodeployment/lib/apache_setup/setup_based_on_template.bash")
 fi
 # set rgb service for faster access and load time
 _msg_ "RUN: set up service if needed - rgb_led_controller"
-(. "/home/$USER/rpitools/gpio/rgb_led/systemd_setup/set_service.bash")
+("/home/$USER/rpitools/gpio/rgb_led/systemd_setup/set_service.bash")
 
 # install and configure motion camera service
 _msg_ "RUN: motion install and configure"
-(. "/home/$USER/rpitools/autodeployment/lib/motion/install_n_configure.bash")
+("/home/$USER/rpitools/autodeployment/lib/motion/install_n_configure.bash")
 
 # motion video stream over apache server setup, run after apache config
 _msg_ "RUN: motion video stream forwarding"
-(. /home/$USER/rpitools/autodeployment/lib/motion_remote_video_stream_over_apache.bash)
+("/home/$USER/rpitools/autodeployment/lib/motion_remote_video_stream_over_apache.bash")
 
 # Set status file under cache
 post_config_actions_done
@@ -128,10 +128,16 @@ then
     _msg_ "USER SCRIPT WAS ACTIVATED"
     _msg_ "\t$($confighandler --user_script)"
     path="$($confighandler -s USER_SPACE -o path)"
-    _msg_ "RUN: bash $path"
-    _msg_ "\n################################## $(date '+%Y.%m.%d %H:%M:%S') ##################################"
-    sudo bash "$path"
-    echo -e "################################## $(date '+%Y.%m.%d %H:%M:%S') ##################################"
+    shellcheck "$path"
+    if [ "$?" -ne 0 ]
+    then
+        _msg_ "\tShellcheck validation failed, please check your script with shellcheck\nand correct the error(s) and warning(s)."
+    else
+        _msg_ "RUN: bash $path"
+        _msg_ "\n################################## $(date '+%Y.%m.%d %H:%M:%S') ##################################"
+        sudo bash "$path"
+        echo -e "################################## $(date '+%Y.%m.%d %H:%M:%S') ##################################"
+    fi
 else
     _msg_ "USER SCRIPT WAS NOT ACTIVATED"
 fi
