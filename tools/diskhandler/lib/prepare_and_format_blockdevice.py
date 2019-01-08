16ce03cd5cafe0d8d55a9aa8580fd5ab907cb10c
ban.marcell@gmail.com <bxnxm>
2019-01-08 22:01:00 +0000

    diskhandler module backend in progress continue

--- tools/diskhandler/lib/prepare_and_format_blockdevice.py
+++ tools/diskhandler/lib/prepare_and_format_blockdevice.py
@@ -3,14 +3,20 @@ import json
 import LocalMachine
 import os
 
+""" This module automaticly format the disk based on diskconf.json """
+
+def module_print(text):
+    print_text = "[ autoformat disk ] " + str(text)
+    print(print_text)
+
 def parse_config_file_from_disk(path, confname="diskconf.json"):
     json_path = str(path) + "/" + str(confname)
     if not os.path.exists(json_path):
-        print("\tPath not exists: " + str(json_path))
+        module_print("\tPath not exists: " + str(json_path))
         return None
     with open(json_path, "r") as f:
         data = json.load(f)
-    print("config: " + str(confname) + " => " + str(data))
+    module_print("config: " + str(confname) + " => " + str(data))
     return data
 
 def write_state_config_file_from_disk(path, data, confname="diskconf.json"):
@@ -32,30 +38,53 @@ def restore_diskconf_file(path, confname="diskconf.json"):
     cmd = "sudo cp {} {}".format(save_path, json_path)
     exitcode, stdout, stderr = LocalMachine.run_command(cmd)
     BlockDeviceHandler.check_exitcode(cmd, exitcode, stderr)
+    cmd = "sudo rm -f {}".format(save_path)
+    exitcode, stdout, stderr = LocalMachine.run_command(cmd)
+    BlockDeviceHandler.check_exitcode(cmd, exitcode, stderr)
+
+def safe_format_disk_check_force_mode(json_data, dev):
+    dev_data_modified = False
+    # disk is not formatted
+    if json_data['is_formatted'] == "False":
+        dev_data = BlockDeviceHandler.get_device_info_data(dev)
+        if json_data['label'] != dev_data['label']:
+            dev_data_modified = True
+        if json_data['format'] != dev_data['filesystem']:
+            dev_data_modified = True
+    if json_data['force'] == "True" and dev_data_modified is False:
+        module_print("[i] Block device paramaters not changed but force mode is ON")
+        return True
+    elif dev_data_modified is True:
+        module_print("[i] Requested block device parameter(s) changed - format")
+        return True
+    else:
+        module_print("[i] Blockdevice format not needed")
+        return False
 
 def format_device_based_on_config_file(dev, premount_path):
-    print("Format device")
+    module_print("Format device")
     diskconf_path = premount_path
     data = parse_config_file_from_disk(diskconf_path)
     if data is not None:
-        if data['is_formatted'] != "True":
-            print("\tSave disk config file before formatting")
+        if safe_format_disk_check_force_mode(data, dev):
+            module_print("\tSave disk config file before formatting")
             save_diskconf_file(diskconf_path)
-            print("\tUnmount device before formatting")
+            module_print("\tUnmount device before formatting")
             BlockDeviceHandler.unmount_device(dev)
-            print("\tFormat device")
+            module_print("\tFormat device")
             BlockDeviceHandler.format_ex4(dev, data['label'])
-            print("\tMount formatted device")
+            module_print("\tMount formatted device")
             mount_point = BlockDeviceHandler.mount_device(dev)
-            print("\tSave back the the config file with the new state")
+            module_print("\tSave back the the config file with the new state")
             write_state_config_file_from_disk(mount_point, data)
         else:
-            print("\tDisk already formatted: {}:{}".format(dev, premount_path))
+            module_print("\tDisk already formatted: {}:{}".format(dev, premount_path))
+    module_print("mount device: " + str(dev))
     mount_point = BlockDeviceHandler.mount_device(dev)
 
 def prepare_block_device():
     if BlockDeviceHandler.is_any_device_avaible():
-        print("Block device exists")
+        module_print("Block device exists")
         devices = BlockDeviceHandler.list_connected_devices()
         for dev in devices:
             premount_path = BlockDeviceHandler.premount_device(dev)
