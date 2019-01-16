1fb920978a12c149cd1881f070679b1065613257
BxNxM <ban.marcell@gmail.com>
2019-01-17 00:03:59 +0100

    motioncontroll status --no-pager

--- tools/motioncontroll.bash
+++ tools/motioncontroll.bash
@@ -6,7 +6,12 @@ function motioncontroll() {
     if [ "$cmd" != "" ]
     then
         echo -e "$(echo $service | awk "{print toupper(\$0)}") CMD: $cmd"
-        sudo systemctl "$cmd" "$service"
+        if [ "$cmd" == "status" ]
+        then
+            sudo systemctl "$cmd" --no-pager "$service"
+        else
+            sudo systemctl "$cmd" "$service"
+        fi
     fi
     echo -e "$(echo $service | awk "{print toupper(\$0)}") STATUS: $(sudo systemctl is-active $service)"
 }
