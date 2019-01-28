0150543c4c0deaacd22accf4b7a8c962e5de06bf
ban.marcell@gmail.com <BxNxM>
2019-01-28 19:58:24 +0000

    motioncontroll service shortcut smarter then ever

--- tools/motioncontroll.bash
+++ tools/motioncontroll.bash
@@ -2,18 +2,33 @@
 
 function motioncontroll() {
     cmd="$1"
+    short_status="$2"
     service="motion"
+
+    if [ "$cmd" == "--help" ] || [ "$cmd" == "-h" ] || [ "$cmd" == "" ]
+    then
+        echo -e "MOTION SERVICE QUICK CONTROLLER"
+        echo -e "start\t\t - start motion camera"
+        echo -e "stop\t\t - stop motion camera"
+        echo -e "status\t\t - show status of motion camera"
+        echo -e "status --short\t - shows short status of motion camera"
+        exit 0
+    fi
+
     if [ "$cmd" != "" ]
     then
         echo -e "$(echo $service | awk "{print toupper(\$0)}") CMD: $cmd"
         if [ "$cmd" == "status" ]
         then
-            sudo systemctl "$cmd" --no-pager "$service"
+            if [ "$short_status" != "--short" ]
+            then
+                sudo systemctl "$cmd" --no-pager "$service"
+            fi
         else
             sudo systemctl "$cmd" "$service"
         fi
+        echo -e "$(echo $service | awk "{print toupper(\$0)}") STATUS: $(sudo systemctl is-active $service)"
     fi
-    echo -e "$(echo $service | awk "{print toupper(\$0)}") STATUS: $(sudo systemctl is-active $service)"
 }
 
 motioncontroll $@
