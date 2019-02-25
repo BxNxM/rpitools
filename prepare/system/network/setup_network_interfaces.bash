4d32035321da85ece17a610143045aaed8502209
ban.marcell@protonmail.com <BxNxM>
2019-02-25 21:35:39 +0000

    setup_network_interfaces printout update

--- prepare/system/network/setup_network_interfaces.bash
+++ prepare/system/network/setup_network_interfaces.bash
@@ -9,6 +9,15 @@ local_orig_path="${MYDIR}/interfaces.orig"
 local_patch_path="${MYDIR}/interfaces.patch"
 etc_interfaces_path="/etc/network/interfaces"
 
-echo -e "Patch $etc_interfaces_path"
-exec "$smart_patch" "$etc_interfaces_path" "$local_patch_path"
+function _msg_() {
+    local msg="$*"
+    local title="${YELLOW}[network interfaces configure]${NC} "
+    echo -e "$title$msg"
+}
 
+_msg_ "Setup network interfaces\nPatch $etc_interfaces_path"
+(exec "$smart_patch" "$etc_interfaces_path" "$local_patch_path")
+_msg_ "$etc_interfaces_path content:"
+sudo cat "$etc_interfaces_path"
+_msg_ "Interfaces and IPs"
+ip a
