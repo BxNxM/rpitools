6250d953217595decb2fb035b2089845233fff10
ban.marcell@protonmail.com <BxNxM>
2019-02-25 20:42:36 +0000

    auto network interfaces configure in progress

--- prepare/system/network/setup_network_interfaces.bash
+++ prepare/system/network/setup_network_interfaces.bash
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+MYPATH="${BASH_SOURCE[0]}"
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${MYDIR}/../../colors.bash"
+
+smart_patch="${MYDIR}/../../../tools/smart_patch_applier.bash"
+local_orig_path="${MYDIR}/interfaces.orig"
+local_patch_path="${MYDIR}/interfaces.patch"
+etc_interfaces_path="/etc/network/interfaces"
+
+echo -e "Patch $etc_interfaces_path"
+exec "$smart_patch" "$etc_interfaces_path" "$local_patch_path"
+
