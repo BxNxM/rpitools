868a1f21f93c040457be2b65757081b063a66c89
ban.marcell@gmail.com <BxNxM>
2019-04-05 22:23:54 +0100

    memdict module test improvements

--- tools/socketmem/testmodule/test_memdict_cmdline_interface.bash
+++ tools/socketmem/testmodule/test_memdict_cmdline_interface.bash
@@ -6,11 +6,26 @@ MEMDICT_CLIENT="${MYDIR}/../lib/clientMemDict.py"
 NAMESPACE="general"
 HEALTH=0
 
+progress_status=0
+function progress_indicator() {
+    local progress=(".    " "..   " "...  " ".... " "....." "    ." "   .." "  ..." " ...." ".....")
+
+    echo -n "${progress[$progress_status]}"
+    progress_status=$(($progress_status+1))
+    if [ "$progress_status" -eq ${#progress[@]} ] || [ "$progress_status" -gt "${#progress[@]}" ]
+    then
+        progress_status=0
+    fi
+    sleep .2
+    #echo -ne "\033[2K"
+    echo -ne "\r"
+}
+
 function console() {
     echo -e "[clinet memdict] $*"
 }
 
-function check() {
+function check_output_line_numbers_match() {
     local input=($@)
     local expected_line_number=${input[0]}
     local expected_line_number_extra=($(echo "$expected_line_number" | fold -w1))
@@ -44,27 +59,126 @@ function check() {
     fi
 }
 
+function check_output_substring() {
+    local output="$1"
+    local substring="$2"
+
+    console "Match substring: [$substring] with output."
+    if [[ "$output" == *"$substring"* ]]
+    then
+        console "\tPASS"
+    else
+        console "\tFAIL"
+    fi
+}
+
 function test_get_data_silence_False() {
+    local substring="$1"
     local cmd="${MEMDICT_CLIENT} -md -n ${NAMESPACE} -k born -s False"
     console "[test_get_data_silence_False] CMD: $cmd"
     output=$($cmd)
     console "[output]\n|$output|"
-    check "1+" "$output"
+    check_output_line_numbers_match "1+" "$output"
+    if [ "$substring" != "" ]
+    then
+        check_output_substring "$output" "$substring"
+    fi
 }
 
 function test_get_data_silence_True() {
+    local substring="$*"
     local cmd="${MEMDICT_CLIENT} -md -n ${NAMESPACE} -k born -s True"
     console "[test_get_data_silence_True] CMD: $cmd"
     output=$($cmd)
     console "[output]\n|$output|"
-    check "1" "$output"
+    check_output_line_numbers_match "1" "$output"
+    if [ "$substring" != "" ]
+    then
+        check_output_substring "$output" "$substring"
+    fi
 }
 
+function run_thread_get_data() {
+    local cnt=0
+    local time_query_sec="$1"
+
+    SECONDS=0
+    while true
+    do
+        test_get_data_silence_True > /dev/null
+        cnt=$(($cnt+1))
+        if [ "$SECONDS" -gt "$time_query_sec" ]
+        then
+            break
+        fi
+    done
+    console "\tREAD TEST: $cnt / $time_query_sec sec"
+}
+
+function test_get_data_parallel_silence_True() {
+    local time_query_sec="$1"
+    local thread="$2"
+    local pid_list=()
+
+    for ((i=0; i<"$thread"; i++))
+    do
+        run_thread_get_data "$time_query_sec" &
+        pid="$!"
+        console "[ $(($i+1))/$thread ] PID $pid"
+        pid_list+=("$pid")
+    done
+
+    console "WAIT FOR THE PIDS..."
+    while true
+    do
+    for pid in ${pid_list[@]}
+    do
+        if ps -p $pid > /dev/null
+        then
+            if [ -z "$end_progress_animation" ]
+            then
+                progress_indicator
+            fi
+        else
+            if [ "$pid" -eq "${pid_list[0]}" ]
+            then
+                end_progress_animation=1
+            fi
+            if [ "$pid" -eq "${pid_list[-1]}" ]
+            then
+                is_break=1
+                break
+            fi
+        fi
+    done
+    if [ ! -z "$is_break" ] && [ "$is_break" -eq 1 ]
+    then
+        break
+    fi
+    done
+
+    # post check
+    for pid in ${pid_list[@]}
+    do
+        wait "${pid_list[@]}"
+    done
+}
+
+
 function main() {
+
     console "---------------------------------------------"
-    test_get_data_silence_False
+    console "------  SIMPLE DATA QUERY FROM MEMDICT  -----"
     console "---------------------------------------------"
-    test_get_data_silence_True
+    test_get_data_silence_False "around2018"
+    console "---------------------------------------------"
+    test_get_data_silence_True "around2018"
+
+    console "---------------------------------------------"
+    console "----  PARALLEL DATA QUERY FROM MEMDICT  -----"
+    console "---------------------------------------------"
+    # sample time, threads
+    test_get_data_parallel_silence_True 10 10
 }
 
 main
