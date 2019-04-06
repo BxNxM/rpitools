fdc92b9657d58782ddc8fb0564db664f26436b46
ban.marcell@gmail.com <BxNxM>
2019-04-06 11:27:43 +0100

    memdict test done - finalized

--- tools/socketmem/testmodule/test_memdict_cmdline_interface.bash
+++ tools/socketmem/testmodule/test_memdict_cmdline_interface.bash
@@ -6,6 +6,9 @@ MEMDICT_CLIENT="${MYDIR}/../lib/clientMemDict.py"
 NAMESPACE="general"
 HEALTH=0
 
+source "${MYDIR}/colors.bash"
+
+# ================================= TOOLS ====================================#
 progress_status=0
 function progress_indicator() {
     local progress=(".    " "..   " "...  " ".... " "....." "    ." "   .." "  ..." " ...." ".....")
@@ -16,13 +19,13 @@ function progress_indicator() {
     then
         progress_status=0
     fi
-    sleep .2
+    sleep .1
     #echo -ne "\033[2K"
     echo -ne "\r"
 }
 
 function console() {
-    echo -e "[clinet memdict] $*"
+    echo -e "${PURPLE}[clinet memdict]${NC} $*"
 }
 
 function check_output_line_numbers_match() {
@@ -52,9 +55,9 @@ function check_output_line_numbers_match() {
 
     if [ "${#lines[@]}" "$operation" "$expected_line_number" ]
     then
-        console "\tPASS"
+        console "\t${GREEN}PASS${NC}"
     else
-        console "\tFAIL"
+        console "\t${RED}FAIL${NC}"
         HEALTH=$(($HEALTH + 1))
     fi
 }
@@ -66,12 +69,14 @@ function check_output_substring() {
     console "Match substring: [$substring] with output."
     if [[ "$output" == *"$substring"* ]]
     then
-        console "\tPASS"
+        console "\t${GREEN}PASS${NC}"
     else
-        console "\tFAIL"
+        console "\t${RED}FAIL${NC}"
+        HEALTH=$(($HEALTH + 1))
     fi
 }
 
+# ================================= TESTS ====================================#
 function test_get_data_silence_False() {
     local substring="$1"
     local cmd="${MEMDICT_CLIENT} -md -n ${NAMESPACE} -k born -s False"
@@ -124,7 +129,7 @@ function test_get_data_parallel_silence_True() {
     do
         run_thread_get_data "$time_query_sec" &
         pid="$!"
-        console "[ $(($i+1))/$thread ] PID $pid"
+        #console "[ $(($i+1))/$thread ] PID $pid"
         pid_list+=("$pid")
     done
 
@@ -164,32 +169,160 @@ function test_get_data_parallel_silence_True() {
     done
 }
 
+function test_write_data_silence_False() {
+    local substring="$1"
+    local cmd="${MEMDICT_CLIENT} -md -n ${NAMESPACE} -k service -s True"
+    default_value=$($cmd)
+    console "Default value: $default_value"
+    local cmd="${MEMDICT_CLIENT} -md -n ${NAMESPACE} -k service -v $substring -s False"
+    console "[test_write_data_silence_False] CMD: $cmd"
+    output=$($cmd)
+    console "[output]\n|$output|"
+    check_output_line_numbers_match "1+" "$output"
+    if [ "$substring" != "" ]
+    then
+        check_output_substring "$output" "$substring"
+    fi
+    console "Restore default value: $default_value"
+}
+
+function test_write_data_silence_True() {
+    local substring="$1"
+    local cmd="${MEMDICT_CLIENT} -md -n ${NAMESPACE} -k service -s True"
+    default_value=$($cmd)
+    console "Default value: $default_value"
+    local cmd="${MEMDICT_CLIENT} -md -n ${NAMESPACE} -k service -v $substring -s True"
+    console "[test_write_data_silence_False] CMD: $cmd"
+    output=$($cmd)
+    console "[output]\n|$output|"
+    check_output_line_numbers_match "1" "$output"
+    if [ "$substring" != "" ]
+    then
+        check_output_substring "$output" "True"
+    fi
+    console "Restore default value: $default_value"
+}
+
+function run_thread_write_data() {
+    local cnt=0
+    local time_query_sec="$1"
+
+    SECONDS=0
+    while true
+    do
+        test_write_data_silence_True > /dev/null
+        cnt=$(($cnt+1))
+        if [ "$SECONDS" -gt "$time_query_sec" ]
+        then
+            break
+        fi
+    done
+    console "\tWRITE TEST: $(($cnt*2)) write / $time_query_sec sec"
+}
+
+function test_write_data_parallel_silence_True() {
+    local time_query_sec="$1"
+    local thread="$2"
+    local pid_list=()
+
+    for ((i=0; i<"$thread"; i++))
+    do
+        run_thread_write_data "$time_query_sec" &
+        pid="$!"
+        #console "[ $(($i+1))/$thread ] PID $pid"
+        pid_list+=("$pid")
+    done
+
+    console "WAIT FOR THE PIDS..."
+    while true
+    do
+    for pid in ${pid_list[@]}
+    do
+        if ps -p $pid > /dev/null
+        then
+            if [ -z "$end_progress_animation" ]
+            then
+                progress_indicator
+            fi
+        else
+            if [ "$pid" -eq "${pid_list[0]}" ]
+            then
+                end_progress_animation=1
+            fi
+            if [ "$pid" -eq "${pid_list[-1]}" ]
+            then
+                is_break=1
+                break
+            fi
+        fi
+    done
+    if [ ! -z "$is_break" ] && [ "$is_break" -eq 1 ]
+    then
+        break
+    fi
+    done
+
+    # post check
+    for pid in ${pid_list[@]}
+    do
+        wait "${pid_list[@]}"
+    done
+}
 
 function main() {
 
     console "---------------------------------------------"
-    console "------  SIMPLE DATA QUERY FROM MEMDICT  -----"
+    console "------  ${PURPLE}SIMPLE DATA QUERY FROM MEMDICT${NC}  -----"
     console "---------------------------------------------"
     test_get_data_silence_False "around2018"
     console "---------------------------------------------"
     test_get_data_silence_True "around2018"
 
     console "---------------------------------------------"
-    console "----  PARALLEL DATA QUERY FROM MEMDICT  -----"
+    console "------  ${PURPLE}SIMPLE DATA WRITE TO MEMDICT${NC}  -----"
+    console "---------------------------------------------"
+    test_write_data_silence_False "rpitools_write_testX"
+    console "---------------------------------------------"
+    test_write_data_silence_True "rpitools_write_testY"
+
+    console "---------------------------------------------"
+    console "----  ${PURPLE}PARALLEL DATA QUERY FROM MEMDICT${NC}  -----"
     console "---------------------------------------------"
     # sample time, threads
-    console "SAMPLE TIME: 1, THREADS: 100"
+    console "${YELLOW}[READ] SAMPLE TIME: 1, THREADS: 100${NC}"
     test_get_data_parallel_silence_True 1 100
-    console "SAMPLE TIME: 5, THREADS: 50"
+    console "---------------------------------------------"
+    console "${YELLOW}[READ] SAMPLE TIME: 5, THREADS: 50${NC}"
     test_get_data_parallel_silence_True 5 50
+    console "---------------------------------------------"
+    console "${YELLOW}[READ] SAMPLE TIME: 1, THREADS: 6${NC}"
+    test_get_data_parallel_silence_True 1 6
+
+    console "---------------------------------------------"
+    console "----  ${PURPLE}PARALLEL DATA WRITE FROM MEMDICT${NC}  -----"
+    console "---------------------------------------------"
+    console "${YELLOW}[WRITE] SAMPLE TIME: 5, THREADS: 3${NC}"
+    test_write_data_parallel_silence_True 5 3
+    console "---------------------------------------------"
+    console "${YELLOW}[WRITE] SAMPLE TIME: 5, THREADS: 10${NC}"
+    test_write_data_parallel_silence_True 5 10
 }
 
 main
+
+console "---------------------------------------------"
+console "----  ${PURPLE}=========== SUMMARY =========${NC}  -----"
+console "---------------------------------------------"
 if [ "$HEALTH" -eq 0 ]
 then
-    console "OVERALL HEALTH GOOD [$HEALTH]"
+    console "${YELLOW}OVERALL HEALTH ${GREEN}GOOD${NC} [$HEALTH]"
 else
-    console "OVERALL HEALTH BAD [$HEALTH]"
+    console "${YELLOW}OVERALL HEALTH ${RED}PROBLEMATIC${NC} [$HEALTH]"
 fi
+
+console "${YELLOW}SHOW MEMDICT CONTENT${NC}"
+${MEMDICT_CLIENT} -sh -s True
+console "${YELLOW}SHOW MEMDICT STATISTIC${NC}"
+${MEMDICT_CLIENT} -st -s True
 
 exit "$HEALTH"
