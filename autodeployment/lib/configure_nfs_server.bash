db7d942867527ae1210ba12b216e7f0606a2f0cb
ban.marcell@gmail.com <BxNxM>
2019-04-10 19:59:21 +0100

    nfs server fix + test client mount point

--- autodeployment/lib/configure_nfs_server.bash
+++ autodeployment/lib/configure_nfs_server.bash
@@ -13,6 +13,9 @@ _msg_title="NFS SETUP"
 nfs_shared_folder="$($confighandler -s NFS_SERVER -o nfs_shared_folder)"
 nfs_shared_folder_permissions="$($confighandler -s NFS_SERVER -o nfs_shared_folder_permissions)"
 
+# =============================================================== #
+#                          SERVER SETUP                           #
+# =============================================================== #
 action=false
 function create_nfs_file_structure() {
 
@@ -21,7 +24,7 @@ function create_nfs_file_structure() {
         _msg_ "Create shared folder: ${nfs_shared_folder}"
         sudo bash -c "mkdir -p  ${nfs_shared_folder}"
 
-        _msg_ "Add permissions: hmod -R ${nfs_shared_folder_permissions} ${nfs_shared_folder}"
+        _msg_ "Add permissions: chmod -R ${nfs_shared_folder_permissions} ${nfs_shared_folder}"
         sudo bash -c "chmod -R ${nfs_shared_folder_permissions} ${nfs_shared_folder}"
 
         action=true
@@ -29,7 +32,6 @@ function create_nfs_file_structure() {
         _msg_ "Shared folder: ${nfs_shared_folder} already was set"
     fi
 }
-
 
 function edit_exports_file_and_permissions() {
     local export_file_content="${nfs_shared_folder} *(rw,sync)"
@@ -54,6 +56,59 @@ function edit_exports_file_and_permissions() {
     fi
 }
 
+# =============================================================== #
+#                        TEST CLIENT SETUP                        #
+# =============================================================== #
+function create_nfs_client_local_mount_point() {
+    local nfs_client_mount_point_path="$1"
+    if [ ! -d "$nfs_client_mount_point_path" ]
+    then
+        _msg_ "Create nfs client local mount point: $nfs_client_mount_point_path"
+        sudo mkdir -p "$nfs_client_mount_point_path"
+        sudo chown -R ${USER}:${USER} "$nfs_client_mount_point_path"
+    else
+        _msg_ "nfs client local mount point exists: $nfs_client_mount_point_path"
+    fi
+}
+function mount_nfs_server() {
+    local ip="$1"
+    local nfs_client_path_to_mount="$2"
+    local nfs_client_mount_point_path="$3"
+    _msg_ "Manual mount: sudo mount ${ip} ${nfs_client_path_to_mount} ${nfs_client_mount_point_path}"
+    sudo mount ${ip}:${nfs_client_path_to_mount} ${nfs_client_mount_point_path}
+}
+function edit_fstab_for_automount() {
+    local ip="$1"
+    local nfs_client_path_to_mount="$2"
+    local nfs_client_mount_point_path="$3"
+    local fstab_setup_cmd="${ip}:${nfs_client_path_to_mount} ${nfs_client_mount_point_path} nfs rw 0 0"
+    local fstab_path="/etc/fstab"
+    (sudo cat "${fstab_path}" | grep "${fstab_setup_cmd}")
+    if [ "$?" -ne 0 ]
+    then
+        _msg_ "Set $fstab_setup_cmd to $fstab_path"
+        sudo bash -c "echo -e ${fstab_setup_cmd} >> ${fstab_path}"
+
+        (sudo cat "${fstab_path}" | grep "${fstab_setup_cmd}")
+        if [ "$?" -eq 0 ]
+        then
+            "Set test mount point for local nfs server was successful"
+        fi
+    else
+        _msg_ "$fstab_setup_cmd already set in $fstab_path"
+    fi
+    mount_nfs_server "$ip" "$nfs_client_path_to_mount" "$nfs_client_mount_point_path"
+}
+function create_test_mount_point() {
+    local nfs_client_mount_point_path="/media/.local_nfs_client"
+    _msg_ "Test client mount point: $nfs_client_mount_point_path"
+    create_nfs_client_local_mount_point "$nfs_client_mount_point_path"
+    edit_fstab_for_automount "localhost" "$nfs_shared_folder" "$nfs_client_mount_point_path"
+}
+
+# =============================================================== #
+#                              MAIN                               #
+# =============================================================== #
 _msg_ "NFS HOWTO: https://www.htpcguides.com/configure-nfs-server-and-nfs-client-raspberry-pi/"
 create_nfs_file_structure
 if [ ! -f "$CACHE_PATH_is_set" ]
@@ -62,3 +117,4 @@ then
 else
     _msg_ "NFS was aleady set: $CACHE_PATH_is_set exists"
 fi
+create_test_mount_point
